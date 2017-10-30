0dba80adfbf1bca282e8a6bc79f73f01bc73d40e
Robert Herschel Hawk <robert@the-hawk.us>
2017-10-29 21:53:58 -0700

    Completed install script

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/vimrc ~/.ideavimrc
+ln -s ~/.vim/gvimrc ~/.gvimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall

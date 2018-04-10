2a59ca135d9750366203bace99f76f0f9236b07a
Robert Herschel Hawk <robert@the-hawk.us>
2018-04-10 12:43:32 -0700

    Automated commit from ender-ii.the-hawk.us

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/vimrc ~/.ideavimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s $HOME/.vim/vimrc $HOME/.vimrc
+ln -s $HOME/.vim/vimrc $HOME/.ideavimrc
+ln -s $HOME/.vim/gvimrc $HOME/.gvimrc
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --all

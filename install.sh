57ca81a4e385c33a1d46b12b5e80b1c34d8007d7
Robert Herschel Hawk <robert@the-hawk.us>
2018-04-11 22:26:51 -0700

    Automated commit from ender-ii

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ ln -s $HOME/.vim/gvimrc $HOME/.gvimrc
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
-./install.py --all
+./install.py --go-completer --js-completer --java-completer

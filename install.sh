15ef61602255aa5e593d80d9c4c7878cbe03ad52
Robert Hawk <robert.hawk@earnin.com>
2019-06-27 12:08:02 -0700

    Added the init.vim to install script
    Since the switch to NeoVim, I've been using suggested init.vim script
    from the :help vim-from-neovim command. I added it to install here.

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+mkdir -p $HOME/.config/nvim/
+cp $HOME/.vim/init.vim $HOME/.config/nvim/
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 ln -s $HOME/.vim/vimrc $HOME/.ideavimrc
 ln -s $HOME/.vim/gvimrc $HOME/.gvimrc
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+nvim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --go-completer --js-completer --java-completer
